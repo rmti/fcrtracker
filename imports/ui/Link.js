855af3f054edde192f0e28a3972118d6d01b3e86
Ron Mousseau <rmousseau@gmail.com>
2017-08-03 23:38:38 -0500

    fixed invoice

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -8,7 +8,7 @@ export default () => {
   return (
       <div className="page-content">
         <LinksListFilters/>
-        <AddClient/>
+        <AddLink/>
         <LinksList/>
       </div>
   );
